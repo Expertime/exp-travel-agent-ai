c58bbad05464fb09d448681865e05469cd8280de
Quentin Chevalier <qchevalier@expertime.com>
2025-03-06 11:05:17 +0100

    added configuration

--- src/app.py
+++ src/app.py
@@ -20,7 +20,10 @@ from botbuilder.core import (
     UserState,
 )
 from botbuilder.core.integration import aiohttp_error_middleware
-from botbuilder.integration.aiohttp import CloudAdapter, ConfigurationBotFrameworkAuthentication
+from botbuilder.integration.aiohttp import (
+    CloudAdapter,
+    ConfigurationBotFrameworkAuthentication,
+)
 
 from openai import AzureOpenAI
 from dotenv import load_dotenv
@@ -39,13 +42,20 @@ from routes.static.static import static_routes
 
 load_dotenv(".env")
 
-def create_app(adapter: CloudAdapter, bot: ActivityHandler, agents_client: AgentsOperations, secret_client: SecretClient) -> web.Application:
+
+def create_app(
+    adapter: CloudAdapter,
+    bot: ActivityHandler,
+    agents_client: AgentsOperations,
+    secret_client: SecretClient,
+) -> web.Application:
     app = web.Application(middlewares=[aiohttp_error_middleware])
     app.add_routes(messages_routes(adapter, bot))
     app.add_routes(directline_routes(secret_client))
     app.add_routes(file_routes(agents_client))
     app.add_routes(static_routes())
     return app
+
 
 config = DefaultConfig()
 
@@ -54,10 +64,14 @@ config = DefaultConfig()
 adapter = CloudAdapter(ConfigurationBotFrameworkAuthentication(config))
 
 # Set up service authentication
-credential = DefaultAzureCredential(managed_identity_client_id=os.getenv("MicrosoftAppId"))
+credential = DefaultAzureCredential(
+    managed_identity_client_id=os.getenv("MicrosoftAppId")
+)
 
 # Key Vault
-secret_client = SecretClient(vault_url=os.getenv("AZURE_KEY_VAULT_ENDPOINT"), credential=credential)
+secret_client = SecretClient(
+    vault_url=os.getenv("AZURE_KEY_VAULT_ENDPOINT"), credential=credential
+)
 
 # Azure AI Services
 aoai_client = AzureOpenAI(
@@ -65,14 +79,12 @@ aoai_client = AzureOpenAI(
     azure_endpoint=os.getenv("AZURE_OPENAI_API_ENDPOINT"),
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
     azure_ad_token_provider=get_bearer_token_provider(
-        credential, 
-        "https://cognitiveservices.azure.com/.default"
-    )
+        credential, "https://cognitiveservices.azure.com/.default"
+    ),
 )
 
 project_client = AIProjectClient.from_connection_string(
-    credential=credential,
-    conn_str=os.getenv("AZURE_AI_PROJECT_CONNECTION_STRING")
+    credential=credential, conn_str=os.getenv("AZURE_AI_PROJECT_CONNECTION_STRING")
 )
 agents_client = project_client.agents
 
@@ -82,7 +94,10 @@ graph_client = GraphClient()
 # Conversation history storage
 storage = None
 if os.getenv("AZURE_COSMOSDB_ENDPOINT"):
-    auth_key=os.getenv("AZURE_COSMOSDB_AUTH_KEY", secret_client.get_secret("AZURE-COSMOS-AUTH-KEY").value)
+    auth_key = os.getenv(
+        "AZURE_COSMOSDB_AUTH_KEY",
+        secret_client.get_secret("AZURE-COSMOS-AUTH-KEY").value,
+    )
     storage = CosmosDbPartitionedStorage(
         CosmosDbPartitionedConfig(
             cosmos_db_endpoint=os.getenv("AZURE_COSMOSDB_ENDPOINT"),
@@ -100,17 +115,20 @@ conversation_state = ConversationState(storage)
 
 dialog = LoginDialog()
 
-assistant_id = create_or_update_agent(agents_client, os.getenv("AZURE_OPENAI_ASSISTANT_NAME"))
+assistant_id = create_or_update_agent(
+    agents_client, os.getenv("AZURE_OPENAI_ASSISTANT_NAME")
+)
 
 # Create the bot
 bot = AssistantBot(
-    conversation_state, user_state, 
-    aoai_client, 
-    agents_client, 
+    conversation_state,
+    user_state,
+    aoai_client,
+    agents_client,
     assistant_id,
-    bing_client, 
-    graph_client, 
-    dialog
+    bing_client,
+    graph_client,
+    dialog,
 )
 app = create_app(adapter, bot, agents_client, secret_client)
 
